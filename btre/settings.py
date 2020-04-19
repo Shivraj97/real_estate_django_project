01f34012ebf265215a2fdf3ff803ad782fa07ed6
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-04-19 21:07:28 +0530

    Updated

--- btre/settings.py
+++ btre/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2t*=d6!87*nd2p7k6@yft-%ltk+0tg(lxs194hx1@)nxqc%s-4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['realestatedjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['realestatedjangoproject.herokuapp.com']
 
 
 # Application definition
